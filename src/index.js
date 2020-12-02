f6b3ada334c8f872b931f61e5cf7f22c4b6c29e2
erik thalen <erik.thalen@hyperisland.se>
2020-12-02 23:42:28 +0100

    simplified resize

--- src/index.js
+++ src/index.js
@@ -6,20 +6,18 @@
 
 import AssetsLoaded from './assets-loaded'
 import Animation from './animation'
-import onResize from './resize'
+import Resize from './resize'
 import Copycat from './copycat'
 import SubjectStyling from './subject-styling'
 import Observer from './observer'
 import Status from './status'
 
-import {hasParent, getStyleRefs, randomInt} from './utils'
+import { hasParent, getStyleRefs, randomInt } from './utils'
 
 class Viscosity {
-  constructor({element, easing, wacky}) {
+  constructor({ element, easing, wacky }) {
     this.subject = element
-    this.easing = wacky
-      ? randomInt(.1, .25)
-      : easing || .3
+    this.easing = wacky ? randomInt(0.1, 0.25) : easing || 0.3
 
     if (!this.subject) {
       return
@@ -27,12 +25,12 @@ class Viscosity {
 
     Observer.observe({
       what: this.subject.parentElement,
-      until: () => Status.get(this.subject) === "is-bound"
+      until: () => Status.get(this.subject) === 'is-bound',
     }).then(AssetsLoaded(this).then(this.init.bind(this)))
 
     Status.set(this.subject, 'is-bound')
 
-    onResize(this.restart.bind(this))
+    Resize(this.restart.bind(this))
   }
 
   init() {
@@ -63,35 +61,36 @@ class Viscosity {
       return
     }
 
-    Copycat.subscribe(this, () => {
-      Observer.observe({
-        what: this.subject.parentElement,
-        until: () => !this.subject.parentElement.querySelector(`[data-id='${Copycat.getId(this)}']`)
-      }).then(() => {
-        Copycat.unsubscribe(this)
-        this.init()
-      })
-    })
-
+    // Copycat.subscribe(this, () => {
+    //   Observer.observe({
+    //     what: this.subject.parentElement,
+    //     until: () => !this.subject.parentElement.querySelector(`[data-id='${Copycat.getId(this)}']`)
+    //   }).then(() => {
+    //     Copycat.unsubscribe(this)
+    //     this.init()
+    //   })
+    // })
+    
     this.destroy()
+    setTimeout(this.init.bind(this), 250)
   }
 
   toggle() {
-    Animation.isRunning(this)
-      ? this.destroy()
-      : this.init()
+    Animation.isRunning(this) ? this.destroy() : this.init()
   }
 }
 
-export default function(args) {
+export default function (args) {
   // selector-string passed
   if (typeof args === 'string') {
-    return [...document.querySelectorAll(args)].map(element => new Viscosity({element}))
+    return [...document.querySelectorAll(args)].map(
+      element => new Viscosity({ element })
+    )
   }
 
   // an element passed
   if (args.tagName) {
-    return new Viscosity({element: args})
+    return new Viscosity({ element: args })
   }
 
   // object is passed
